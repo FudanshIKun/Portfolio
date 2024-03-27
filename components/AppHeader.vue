44f86dd369b45e42d559eb167a064d60705ad4f9
Torukun <tracerceser@gmail.com>
2024-03-27 14:33:57 +0700

    Initial commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,89 @@
+<template>
+    <header id="navbar" class="w-full hidden lg:flex flex-col">
+      <nav class="w-full flex justify-between border-bot" style="align-items: center;">
+
+          <div class="flex">
+            <RouterLink id="nav-logo" to="/">
+              {{ config.dev.logo_name }}
+            </RouterLink>
+
+            <RouterLink id="nav-link" to="/" :class="{ active: isActive('/') }">
+              Home
+            </RouterLink>
+  
+            <RouterLink id="nav-link" :to="{name: 'about-me' }" :class="{ active: isActive('/about-me') }">
+              About Me
+            </RouterLink>
+  
+            <RouterLink id="nav-link" to="/projects" :class="{ active: isActive('/projects') }">
+              My Journey
+            </RouterLink>
+          </div>
+
+          <RouterLink id="nav-link-contact" to="/contact-me" :class="{ active: isActive('/contact-me')}">
+           Contact me
+          </RouterLink>
+      
+      </nav>
+
+    </header>
+
+</template>
+
+<style>
+
+#nav-link {
+  @apply text-menu-text font-fira_retina px-6 h-full flex items-center;
+}
+
+#nav-link-contact {
+  @apply text-menu-text font-fira_retina px-6 h-full flex items-center;
+}
+
+#nav-link:hover, #nav-link-contact:hover {
+  color: white;
+}
+
+#nav-logo {
+  @apply text-menu-text font-fira_retina px-6 h-full flex items-center;
+}
+
+#nav-logo:hover {
+  color: white;
+}
+
+#nav-link.router-link-active, #nav-link-contact.router-link-active {
+  color: white;
+}
+
+#nav-logo.router-link-active {
+  border-bottom: none;
+  @apply text-menu-text;
+}
+
+#navbar > nav {
+  height: 45px;
+  font-size: 13px;
+}
+
+</style>
+
+<script>
+
+export default {
+  computed: {
+    // Set active class to current page link
+    isActive() {
+      return route => this.$route.path === route;
+    }
+  },
+  setup() {
+    const config = useRuntimeConfig()
+
+    return {
+      config
+    }
+  },
+};
+
+</script>
---
+++ components/CommentedText.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="code-container flex font-fira_retina text-menu-text">
+    
+
+    <!-- text -->
+    <div class="text-container">
+      <p v-html="text"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      lines: 0
+    };
+  },
+  mounted() {
+    this.updateLines();
+    window.addEventListener("resize", this.updateLines);
+    window.addEventListener("click", this.updateLines);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.updateLines);
+    window.removeEventListener("click", this.updateLines);
+  },
+  methods: {
+    updateLines() {
+      const textContainer = this.$el.querySelector(".text-container");
+      const style = window.getComputedStyle(textContainer);
+      const fontSize = parseInt(style.fontSize);
+      const lineHeight = parseInt(style.lineHeight);
+      const maxHeight = textContainer.offsetHeight;
+      this.lines = Math.ceil(maxHeight / lineHeight) + 1;
+    }
+  }
+};
+</script>
+
+<style>
+.code-container {
+  display: flex;
+  align-items: flex-start;
+}
+
+.line-numbers {
+  text-align: right;
+}
+
+.text-container {
+  width: 100%;
+  padding-left: 10px;
+  word-wrap: break-word;
+}
+</style>
